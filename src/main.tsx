057a356fc0deb66f29fb0a2671a6fcfd8b3981d6
Pcjmy <1716045191@qq.com>
2024-01-30 23:21:03 +0800

    feat: react-router

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+function ReactDemo() {
+  return <h1>React</h1>
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<App />}></Route>
+      <Route path='/react' element={<ReactDemo />}></Route>
+    </Routes>
+  </BrowserRouter>
 )
