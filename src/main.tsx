c09ec6529ff1ff5d5740bf172730b4c030cec4a7
Pcjmy <1716045191@qq.com>
2024-01-30 23:30:53 +0800

    feat: 路由跳转

--- src/main.tsx
+++ src/main.tsx
@@ -8,11 +8,16 @@ function ReactDemo() {
   return <h1>React</h1>
 }
 
+function Vite() {
+  return <h1>Vite</h1>
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <Routes>
       <Route path='/' element={<App />}></Route>
       <Route path='/react' element={<ReactDemo />}></Route>
+      <Route path='/vite' element={<Vite />}></Route>
     </Routes>
   </BrowserRouter>
 )
